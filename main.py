808d4b4272d98898f9abdfc15210a0d2903fa07d
Luke <luke11court@outlook.com>
2021-04-11 13:52:38 +0100

    Beginning of the download module
    In this commit i have rebuilt the original download module to now use
    the ttk modern system themed widgets

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import os
 
-colourpalette: list = ["#005BE8", "#009EF2", "#0BCADB", "#00F2C3", "#00E879"]
+class DynamicDigital:
 
-def exceute_download_section():
-    with open("download.py", "r", encoding="utf-8") as file:
-        exec(file.read())
+    def __init__(self, root):
 
-app = tk.Tk()
-app.geometry("300x400")
-app.configure(bg=colourpalette[0])
-b1 = tk.Button(text="Download", command=exceute_download_section)
-b1.pack()
-tk.mainloop()
+        root.title("Dynamic Digital Server Tools")
+
+        baseframe = ttk.Frame(root, padding="3 3 12 12")
+        baseframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        username_label = ttk.Label(baseframe, text="Enter Username: ")
+        username_label.grid(column=1, row=1, sticky=(W, E))
+
+        self.username = StringVar()
+        username_entry = ttk.Entry(baseframe, width=20, textvariable=self.username)
+        username_entry.grid(column=2, row=1, sticky=(W, E))
+
+        serveraddress_label = ttk.Label(baseframe, text="Enter Server Address: ")
+        serveraddress_label.grid(column=1, row=2, sticky=(W, E))
+
+        self.serveraddress = StringVar()
+        serveraddress_entry = ttk.Entry(baseframe, width=20, textvariable=self.serveraddress)
+        serveraddress_entry.grid(column=2, row=2, sticky=(W, E))
+
+        downloadaddress_label = ttk.Label(baseframe, text="Enter Download Address: ")
+        downloadaddress_label.grid(column=1, row=3, sticky=(W, E))
+
+        self.downloadaddress = StringVar()
+        downloadaddress_entry = ttk.Entry(baseframe, width=20, textvariable=self.downloadaddress)
+        downloadaddress_entry.grid(column=2, row=3, sticky=(W, E))
+
+        startdownload = ttk.Button(baseframe, text="Start Download On Server", command=self.download)
+        startdownload.grid(column=2, row=4, sticky=(W, E))
+
+        for child in baseframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+
+    def download(self, *args):
+        usedownloadaddress = self.downloadaddress.get()
+        useserveraddress = self.serveraddress.get()
+        useUserName = self.username.get()
+        os.popen("ssh " + useUserName + "@" + useserveraddress + " wget " + usedownloadaddress)
+
+root = Tk()
+DynamicDigital(root)
+root.mainloop()
